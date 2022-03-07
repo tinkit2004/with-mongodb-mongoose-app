4db2ba147422d68afcc909beb7f45057e0de4063
Andy Fung <tinkit2004@gmail.com>
2022-03-07 12:23:34 +0800

    Add back the LinkedIn Link

--- components/About/about.js
+++ components/About/about.js
@@ -60,6 +60,8 @@ export default function About() {
           <CustomButton
             textHoverGradientColor={linearGradient}
             buttonHoverColor="white"
+            as="a"
+            href="https://www.linkedin.com/in/andy-fung-739a15123"
           >
             Connect me on LinkedIn
           </CustomButton>
